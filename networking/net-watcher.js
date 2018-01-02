115469b48486cff13c2f97a983133e253ce1824d
Rahisul Haque <rahisul@Rahisuls-MacBook-Air.local>
2018-01-02 12:11:33 -0800

    networking files

---
+++ networking/net-watcher.js
@@ -0,0 +1,40 @@
+/******************************************************************************************/
+/*                                                                                        */
+/*                   Rahisul Haque <rahisul@icloud.com>                                   */
+/*                                                                                        */
+/******************************************************************************************/
+
+"use strict"
+
+const 
+	fs = require('fs'),
+	net= require('net'),
+
+
+	filiname = process.argv[2],
+	server = net.createServer(function(connection){
+	//reporting
+	console.log('Subsriber connected');
+	connection.write('Now watching' + filename + ' for change.\n');
+
+	//watch setup
+	let watcher =  fs.watch(filename, function(){
+		connection.write("File named '" + filename + "' changed : " + Date.now() + ".\n");
+	});
+
+
+	//clean up
+	connection.on('close', function(){
+		console.log('Subscriber has been disconnected');
+		watcher.close();
+	 });
+	});
+if (!filename){
+	throw Error('No target file has been found!');
+}
+
+server.listen(5432, function(){
+	console.log('Listening for Subscriber....\n');
+});
+
+
