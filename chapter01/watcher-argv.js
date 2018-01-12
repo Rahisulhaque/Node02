e35d77975c543f22905d9ae2fbdf0417630704f1
Rahisul Haque <rahisul@Rahisuls-MacBook-Air.local>
2018-01-12 13:27:41 -0800

    file watcher with cmd line arg

--- chapter01/watcher-argv.js
+++ chapter01/watcher-argv.js
@@ -1,6 +1,6 @@
 /******************************************************************************/
 /*                                                                            */
-/*                  NodeJS file watcher!                                      */
+/*                  NodeJS file watcher with command line argument            */
 /*                    By Rahisul                                              */
 /*                  rahisul@icloud.com                                        */
 /*                                                                            */
@@ -11,10 +11,17 @@
 
 
 
-const fs = require('fs');
+const 
+	fs = require('fs'),
+	filename = process.argv[2];
+if (!filename){
+	throw Error("This program hasn't found any matching file name.\n");} 
 
-fs.watch('Sample.txt', () => {
+fs.watch(filename, () => {
 	console.log("The file has been changed!");
 	});
+console.log("filewatcher-argv is wokring. It is watching for change of " + filename);
+
+
 
 console.log("fs is watching file... ...\n");
